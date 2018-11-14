d5d89601dd2231b5c2fd77bc321463da65b0a033
Roopa <roopab77@yahoo.com>
2018-11-13 23:12:15 -0500

    Updates

---
+++ precache-manifest.85be965c6d975fd1a1e08ec10779c15b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4514e8bfae977c59d78a",
+    "url": "/clickymemorygame/static/js/runtime~main.4514e8bf.js"
+  },
+  {
+    "revision": "b491fe4c8fda08b631ae",
+    "url": "/clickymemorygame/static/js/main.b491fe4c.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/clickymemorygame/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "36601ed86d331c1167cabe365ddb787b",
+    "url": "/clickymemorygame/index.html"
+  }
+];
